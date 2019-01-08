c3cb71efcb79bccc7e5e99988165cfc94b0e5a7c
Sean Markin <seanmarkin@gmail.com>
2019-01-07 21:09:59 -0800

    Created basic hangman file

---
+++ hangman1.py
@@ -0,0 +1,9 @@
+# concern : how do we represent the word along with an indication of what letters have been guessed?
+
+word = [("a", False), ("n", False), ("i", False)]
+word = [("a", True), ("n", False), ("i", True)]
+print(word)
+
+print([2*i for i in range(15) if i%2 == 0])
+print([t[0] for t in word if t[1] == True])
+# x | x <- R, x > 0
