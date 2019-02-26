a77cb055e3b57724c152e2b0d9fc4007b3f14cf4
Mike Mossey <michaelmossey@gmail.com>
2019-02-26 13:17:18 -0800

    lesson Feb 25

--- hangman_oo.py
+++ hangman_oo.py
@@ -4,7 +4,15 @@ class WordPule:
 
     def __init__(self, chars):
         # chars "antidisestablishmentarism"
-        self.tuples = [("a", False), ("b")]
+        # self.tuples = [["a", False], ["b", True]]
+        self.stringy = [[x, False] for x in chars]
+
+    def __str__(self):
+        return str(self.stringy)
+
+    def toGameString(self):
+        return "b _ n _ n _ "
+
 
     def numGuessed(self):
         """returns number of successfully guessed letters"""
@@ -21,3 +29,8 @@ class WordPule:
 def loop():
     # instantiate a WordPule and manipulate it to run the game
     pass
+
+wordup = WordPule("dog")
+print(wordup.stringy)
+wordup.stringy = 7 # result in a compiler error
+wordup.runGame()
