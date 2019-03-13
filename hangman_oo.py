05ec2d527ed9341d836dfc767234301b03eb5c6f
Sean Markin <seanmarkin@gmail.com>
2019-03-12 21:11:41 -0700

    Updated with additional methods and functions

--- hangman_oo.py
+++ hangman_oo.py
@@ -30,9 +30,67 @@ class WordPule:
         """mutates self.tuples"""
         pass
 
-def loop():
-    # instantiate a WordPule and manipulate it to run the game
+    def haveWon(self):
+        pass
+
+def getWord():
+    return "mike"
+
+def checkLetter():
     pass
+
+def start():
+    """Initialize WordPule and return it."""
+    newWord = WordPule(getWord())
+    print(newWord.toGameString())
+    return newWord
+
+def loop(w):
+    guesses = 0
+
+    while True:
+        guessedLetter = input("Guess a letter")
+        # this employee's job (this function) will be
+        # to determine if it's a correct guess (alter 'w'
+        # if so) and whether game has been won or lost
+        isCorrect = w.checkLetter(guessedLetter) # delegate this to WordPule
+        if isCorrect:
+            print ("Awesome!!!")
+        else:
+            print ("too bad")
+
+        if w.haveWon():
+            isWin = True
+            return isWin
+
+        elif guesses <= 3 + w.wordLength():
+            isWin = False
+            guesses += 1
+
+        else:
+            isWin = False
+            return isWin
+
+    # Check guessed letter to see if its in the word
+    # If its in the word --> change all instances of letter in stringy to True
+    # If its not in the word -->
+
+def finish(w, winFlag):
+    if winFlag:
+        print("YOU WIN!")
+    else:
+        print("YOU LOSE")
+
+    if input("Would you like to play again True/False?"):
+        main()
+    else:
+        break
+
+def main():
+    # instantiate a WordPule and manipulate it to run the game
+    w = start()
+    winFlag = loop(w)
+    finish(w, winFlag)
 
 wordup = WordPule("dog")
 print(wordup.toGameString())
