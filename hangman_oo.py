9c7d931c99103a9c3685374cbd47562c63b0034d
Sean Markin <seanmarkin@gmail.com>
2019-03-23 10:46:01 -0700

    Updated multiple methods including haveWon.  Game currently works although it allows you to guess an unlimited number of correct guesses.

--- hangman_oo.py
+++ hangman_oo.py
@@ -1,11 +1,11 @@
 
 class WordPule:
 
-
     def __init__(self, chars):
         # chars "antidisestablishmentarism"
         # self.tuples = [["a", False], ["b", True]]
         self.stringy = [[x, False] for x in chars]
+        print(self.stringy)
 
     def __str__(self):
         return str(self.stringy)
@@ -31,13 +31,24 @@ class WordPule:
         pass
 
     def haveWon(self):
-        pass
+        hasWon = True
+        for i in range(len(self.stringy)):
+            if self.stringy[i][1] == False:
+                hasWon  = False
+        return hasWon
+
+    def checkLetter(self, letterToCheck):
+        isLetterInWord = False
+        for i in range(len(self.stringy)):
+            if self.stringy[i][0] == letterToCheck:
+                isLetterInWord  = True
+                self.stringy[i][1] = True
+        return  isLetterInWord
 
 def getWord():
     return "mike"
 
-def checkLetter():
-    pass
+
 
 def start():
     """Initialize WordPule and return it."""
@@ -46,51 +57,58 @@ def start():
     return newWord
 
 def loop(w):
-    guesses = 0
+    wrongGuesses = 0
 
     while True:
+        print(wrongGuesses)
         guessedLetter = input("Guess a letter")
         # this employee's job (this function) will be
         # to determine if it's a correct guess (alter 'w'
         # if so) and whether game has been won or lost
         isCorrect = w.checkLetter(guessedLetter) # delegate this to WordPule
+
         if isCorrect:
             print ("Awesome!!!")
         else:
-            print ("too bad")
+            wrongGuesses += 1
+            if wrongGuesses >= 3:
+                print("You Lose")
+                break
+            else:
+                print ("too bad, guess again")
 
         if w.haveWon():
             isWin = True
-            return isWin
+            print("You Win")
+            break
+            #return isWin
+        #else:
+            #isWin = False
+            #return isWin
 
-        elif guesses <= 3 + w.wordLength():
-            isWin = False
-            guesses += 1
-
-        else:
-            isWin = False
-            return isWin
 
     # Check guessed letter to see if its in the word
     # If its in the word --> change all instances of letter in stringy to True
     # If its not in the word -->
 
-def finish(w, winFlag):
-    if winFlag:
-        print("YOU WIN!")
-    else:
-        print("YOU LOSE")
-
-    if input("Would you like to play again True/False?"):
-        main()
-    else:
-        break
+# def finish(w, winFlag):
+#    if winFlag:
+#        print("YOU WIN!")
+#    else:
+#        print("YOU LOSE")
+#
+#    if input("Would you like to play again True/False?"):
+#        main()
+#    else:
+#        break
 
 def main():
     # instantiate a WordPule and manipulate it to run the game
     w = start()
     winFlag = loop(w)
-    finish(w, winFlag)
+    #finish(w, winFlag)
 
-wordup = WordPule("dog")
-print(wordup.toGameString())
+#wordup = WordPule("dog")
+#print(wordup.toGameString())
+
+main()
