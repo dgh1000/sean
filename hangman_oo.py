8c1b1829cdce2ac320ebe1fdb676ae9ee175a92e
Sean Markin <seanmarkin@gmail.com>
2019-03-24 10:44:37 -0700

    New dictionary file

--- hangman_oo.py
+++ hangman_oo.py
@@ -5,6 +5,7 @@ class WordPule:
         # chars "antidisestablishmentarism"
         # self.tuples = [["a", False], ["b", True]]
         self.stringy = [[x, False] for x in chars]
+        self.guessedLetter = []
         print(self.stringy)
 
     def __str__(self):
@@ -14,21 +15,7 @@ class WordPule:
         # return " ".join(<list>)
         # --> ["d", False]
         return " ".join([i if j else "_" for i,j in self.stringy])
-
         #return "b _ n _ n _ "
-
-
-    def numGuessed(self):
-        """returns number of successfully guessed letters"""
-        pass
-
-    def wordLength(selfs):
-        """return number of characters"""
-        pass
-
-    def setLetter(self, letter):
-        """mutates self.tuples"""
-        pass
 
     def haveWon(self):
         hasWon = True
@@ -39,16 +26,26 @@ class WordPule:
 
     def checkLetter(self, letterToCheck):
         isLetterInWord = False
+        self.guessedLetter.append(letterToCheck)
         for i in range(len(self.stringy)):
             if self.stringy[i][0] == letterToCheck:
                 isLetterInWord  = True
                 self.stringy[i][1] = True
         return  isLetterInWord
 
+    def didTheyGuess(self,letterCheck):
+        if letterCheck in self.guessedLetter:
+            return True
+        else:
+            return False
+
+
+
 def getWord():
+
+
+
     return "mike"
-
-
 
 def start():
     """Initialize WordPule and return it."""
@@ -61,10 +58,18 @@ def loop(w):
 
     while True:
         print(wrongGuesses)
-        guessedLetter = input("Guess a letter")
+
         # this employee's job (this function) will be
         # to determine if it's a correct guess (alter 'w'
         # if so) and whether game has been won or lost
+        #alreadyGuessed = True
+        while True:
+            guessedLetter = input("Guess a letter")
+            if w.didTheyGuess(guessedLetter):
+                print("you already guessed this letter, guess again")
+            else:
+                break
+
         isCorrect = w.checkLetter(guessedLetter) # delegate this to WordPule
 
         if isCorrect:
@@ -102,13 +107,30 @@ def loop(w):
 #    else:
 #        break
 
+def loadDictionary():
+    # you want automatic clean up
+    with open("engmix.txt", "rb") as f:
+        #buffer = f.read()
+        # "accused\naccumulate..."
+        words = f.readlines()
+    print(words[1000])
+    return words
+
+
 def main():
     # instantiate a WordPule and manipulate it to run the game
-    w = start()
-    winFlag = loop(w)
+
+    # ["adductors", "adducts", "adeeming", ...]
+    dictWords = loadDictionary()
+    while True:
+        w = start(dictWords)
+        winFlag = loop(w)
+        if theydontwanttoplayagain:
+            break
+
     #finish(w, winFlag)
 
 #wordup = WordPule("dog")
 #print(wordup.toGameString())
 
-main()
+loadDictionary()
