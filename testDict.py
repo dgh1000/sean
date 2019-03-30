dff52f434f190550ff77822f9c22c8b270fd2f73
Mike Mossey <michaelmossey@gmail.com>
2019-03-30 11:26:34 -0700

    test dictionary

---
+++ testDict.py
@@ -0,0 +1,19 @@
+
+def tryDecode(l):
+    try:
+        return l.decode('utf-8')
+    except:
+        return None
+
+with open("engmix.txt", "rb") as f:
+    lines = f.readlines()
+
+linesD = []
+for l in lines:
+    try:
+        x = l.decode("utf-8")
+        linesD.append(x)
+    except:
+        pass
+    
+print(linesD[50001])
