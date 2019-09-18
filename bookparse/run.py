5c08addf078fc3b73706ad7a5737ebb6414a9762
Sean Markin <seanmarkin@gmail.com>
2019-09-18 14:22:56 -0700

    weather.txt

---
+++ bookparse/run.py
@@ -0,0 +1,10 @@
+
+# results = re.finditer(...)
+len(list(results))
+iterlen()
+
+https://docs.python.org/2/library/itertools.html
+https://www.fulltextarchive.com/
+
+https://www.weather.gov/rev/Text_Forecasts
+figure out city, time, wind speed... any standardized formatted information
